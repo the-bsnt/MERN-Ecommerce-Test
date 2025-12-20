728c5c5dacf0214fac3d2b04da90f4c84637574a
Hari Aryal <aryalhari059@gmail.com>
2025-12-20 21:32:42 +0545

    models

--- server/server.js
+++ server/server.js
@@ -10,6 +10,7 @@ const adminProductsRouter= require('./routes/admin/products-routes');
 const shopProductsRouter = require('./routes/shop/products-routes');
 const shopCartRouter= require('./routes/shop/cart-routes');
 const shopAddressRouter = require("./routes/shop/address-routes");
+const shopOrderRouter=require('./routes/shop/order-routes')
 mongoose
   .connect(
     "mongodb+srv://aryalhari059:hari%402060@cluster0.edn2tbq.mongodb.net/?retryWrites=true&w=majority"
@@ -21,18 +22,20 @@ mongoose
 
 
   const PORT = process.env.PORT || 5000;
-app.use(cors({
-    origin:'https://frontend-ymwa.onrender.com',
-    methods:['GET','POST','DELETE','PUT'],
-    allowedHeaders:[
-        "Content-Type",
-        'Authorization',
-        'Cache-Control',
-        'Expires',
-        'Pragma'
+app.use(
+  cors({
+    origin: "https://ecommerce-im7c.onrender.com",
+    methods: ["GET", "POST", "DELETE", "PUT"],
+    allowedHeaders: [
+      "Content-Type",
+      "Authorization",
+      "Cache-Control",
+      "Expires",
+      "Pragma",
     ],
-    credentials: true
-}));
+    credentials: true,
+  })
+);
 
   app.use(cookieParser());
   app.use(express.json());
@@ -42,5 +45,6 @@ app.use(cors({
    app.use('/api/shop/products', shopProductsRouter);
    app.use('/api/shop/cart', shopCartRouter);
    app.use("/api/shop/address", shopAddressRouter);
+   app.use("/api/shop/order", shopOrderRouter);
   app.listen(PORT,()=>console.log("Server Running"));
 
