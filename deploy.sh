cd28d1fd1bf969291b0bc96e7a3de3ecda954d3d
Basnet Sameer <the.bsnt.official@gmail.com>
2026-01-20 13:13:49 +0545

    pipeline

--- deploy.sh
+++ deploy.sh
@@ -4,23 +4,26 @@
 set -e
 
 # Go to client directory (important for npm)
-cd /root/home/MERN-Ecommerce/client
+cd /home/esten/MERN-Ecommerce-Test/client
 
+npm ci
+
+npm audit fix
 # Build React app
 npm run build
 
 
 # Copy new build to nginx html directory
-cp -r dist/* /usr/share/nginx/html/
+sudo cp -r dist/* /usr/share/nginx/html/
 
 echo "Build completed and files copied to Nginx successfully."
- 
+
 # Rebuild and Compose docker if changes in .env are made
 
 echo "Rebuilding docker"
 
 cd ..
 
-docker compose -f docker-compose.prod.yml up --build -d 
+docker compose -f docker-compose.prod.yml up --build -d
 
 echo "Docker successfully composed"
